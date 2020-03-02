1a5844953af4d14116a13d766c0c1516162ebc7b
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2020-03-02 10:34:40 +0100

    Fin login
    Maintenant l'email ou le pseudo marche

--- model/userManagment.php
+++ model/userManagment.php
@@ -2,12 +2,35 @@
 
 require "dbConnector.php";
 
+//cette fonction regarde si le login inscrit est juste
+function checkLogin($post)
+{
+
+    //cette requete sert a vérifier si ce qu'il a entrer comme username ou pseudo corresond a quelque chose dans la BD car on ne peut plus vérifier le password vu qu'il est hasher
+    $requete = "SELECT userEmailAddress, userPsw, pseudo FROM users where userEmailAddress = '" . @$post['username']. "' OR pseudo ='".@$post['username']. "';";
+
+    $result = executeQuery($requete);
+
+
+    if ($result) {
+        $passwordHash = $result[0]["userPsw"];
+        if (password_verify(@$post['password'], $passwordHash)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else
+
+        return false;
+
+}
+
 
 //cette fonction va créer un compte qui n'existe pas
 function creatUser($post)
 {
 
-    $requeteCreateUser = "Select userEmailAddress, pseudo from users where userEmailAddress ='" . @$post['usernameRegister'] . "' and pseudo = '" . @$post['pseudoRegister'] . "';";
+    $requeteCreateUser = "Select userEmailAddress, pseudo from users where userEmailAddress ='" . @$post['usernameRegister'] . "' or pseudo = '" . @$post['pseudoRegister'] . "';";
     $result = executeQuery($requeteCreateUser);
 
 
@@ -20,7 +43,9 @@ function creatUser($post)
 
 
             $passwordHash = password_hash(@$post['passwordRegister'], PASSWORD_DEFAULT);
-            $requeteCreate = "INSERT INTO users (userEmailAddress, userPsw, pseudo) VALUES ('" . @$post['usernameRegister'] . "','" . $passwordHash . "','" . @$post['pseudoRegister'] . "');";
+            $requeteCreate = "INSERT INTO users (userEmailAddress, userPsw, pseudo) VALUES ('" . @$post['usernameRegister']  . "','" . $passwordHash . "','" . @$post['pseudoRegister'] . "');";
+
+
             executeQuery($requeteCreate);
             return true;
         } else {
@@ -29,29 +54,4 @@ function creatUser($post)
         }
 
     }
-}
-
-//cette fonction regarde si le login inscrit est juste
-function checkLogin($post)
-{
-
-    //cette requete sert a vérifier si ce qu'il a entrer comme username ou pseudo corresond a quelque chose dans la BD car on ne peut plus vérifier le password vu qu'il est hasher
-    $requete = "SELECT userEmailAddress, userPsw, pseudo FROM users where userEmailAddress = '" . @$post['username']. "' OR pseudo ='" .@$post['pseudo']. "';";
-
-    $result = executeQuery($requete);
-
-
-
-    if ($result) {
-        $passwordHash = $result[0]["userPsw"];
-
-        if (password_verify(@$post['password'], $passwordHash)) {
-            return true;
-        } else {
-            return false;
-        }
-    } else
-
-        return false;
-
 }
